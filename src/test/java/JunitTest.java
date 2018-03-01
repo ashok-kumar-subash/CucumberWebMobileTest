2d8e44974501a36837dd9e3838c432e3b758f092
Ashok <ashok.subash@gmail.com>
2018-03-01 17:02:36 +1100

    gecho driver added and updated

--- src/test/java/JunitTest.java
+++ src/test/java/JunitTest.java
@@ -1,13 +1,22 @@
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.WebDriver;
 
-
-
 public class JunitTest {
+    private WebDriver driver ;
     @Test
     public void testBrowser()
     {
+        System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"\\src\\main\\resources\\geckodriver.exe");
+        driver = new FirefoxDriver();
+        driver.get("http://google.com");
+        Assert.assertEquals( "Google",driver.getTitle());
+    }
 
+    @After
+    public void tearDown(){
+        driver.quit();
     }
 }
